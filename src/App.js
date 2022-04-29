89113775400667c46fccb3a6e389e11c6d781ed4
ParthShiroya <parthshiroya98@gmail.com>
2022-04-29 19:23:28 +0200

    Pushed by Parth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./comp/Footer";
@@ -9,15 +13,27 @@ import Login from "./comp/Login";
 import Signup from "./comp/Signup";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
+import Emj from "./pages/emj";
 import Home from "./pages/Home";
 import JobList from "./pages/JobList";
 import Learn from "./pages/Learn";
 import Org from "./pages/Organization";
 import PNF from "./pages/PNF";
-import Emj from "./pages/emj";
+import Button from "@mui/material/Button";
 
 
 const App = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+      document.body.style.pointerEvents = "none";
+      document.getElementById("modal").style.pointerEvents = "auto";
+    }, 5000);
+  }, []);
+  // setShow(true);
+
   useEffect(() => {
     AOS.init();
     AOS.refresh();
@@ -25,6 +41,44 @@ const App = () => {
 
   return (
     <div>
+      {show ? (
+        <div id="modal">
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{
+              width: "100vw",
+              height: "100vh",
+              backgroundColor: "#0004",
+              zIndex: "10",
+            }}
+          >
+            <Paper
+              id="modal-content"
+              className="d-flex flex-column align-items-center justify-content-center position-relative p-5 text-center"
+              elevation={12}
+            >
+              <IconButton
+                aria-label="delete"
+                size="large"
+                color="error"
+                className="position-absolute top-0 end-0"
+                onClick={() => {
+                  setShow(false);
+                  document.body.style.pointerEvents = "auto";
+                  document.getElementById("modal").style.pointerEvents = "none";
+                }}
+              >
+                <CancelOutlinedIcon fontSize="inherit" />
+              </IconButton>
+              <Typography variant="h4">
+                Upload your Resume now, and get your first interview call in <span className="text-grad" style={{fontWeight: "bold"}}>14 DAYS</span>. 
+              </Typography>
+              <Button className="mt-5" variant="outlined" size="large">Upload your Resume</Button>
+            </Paper>
+          </div>
+        </div>
+      ) : null}
+
       {Header()}
 
       <Routes>
